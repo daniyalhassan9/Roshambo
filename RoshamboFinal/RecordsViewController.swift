882f2c6a84c4f0d10d70648ce05095b333b7b150
Danyal Hassan <danoyalhassan93@gmail.com>
2020-04-15 21:43:29 -0400

    Rock Finished

---
+++ RoshamboFinal/RecordsViewController.swift
@@ -0,0 +1,69 @@
+//
+//  RecordsViewController.swift
+//  RoshamboFinal
+//
+//  Created by Danyal Hassan on 4/15/20.
+//  Copyright © 2020 Danyal Hassan. All rights reserved.
+//
+
+import UIKit
+
+class RecordsViewController: UIViewController {
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBOutlet weak var imageRock: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        rock()
+
+        // Do any additional setup after loading the view.
+    }
+    func opponenetMove() -> String {
+           let number = Int.random(in: 1 ..< 4)
+           print(number)
+           
+           switch number {
+           case 1:
+               return "rock"
+           case 2:
+               return "paper"
+           case 3:
+               return "scissors"
+           default:
+               return "invalid"
+           }
+       }
+    
+    func rock() {
+        let oppMove = opponenetMove()
+        
+        switch oppMove {
+        case "paper":
+            result.text = ("paper beats rock")
+            imageRock.image = UIImage(named: "PaperCoversRock")
+        case "rock":
+            result.text = ("tie")
+             imageRock.image = UIImage(named: "itsATie")
+        case "scissors":
+            result.text = ("rock beats scissor")
+             imageRock.image = UIImage(named: "RockCrushesScissors")
+        default:
+            print("invalid value")
+        }
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RoshamboFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RoshamboFinal
+//
+//  Created by Danyal Hassan on 4/15/20.
+//  Copyright © 2020 Danyal Hassan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
